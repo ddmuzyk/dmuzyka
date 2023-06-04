98020b4e14bc40291ce3c8c1a451eb855e197154
ddmuzyk <domino2k1@gmail.com>
2023-06-04 20:21:07 +0200

    change link btns

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,15 +7,14 @@ const Contact = () => {
 
     return (
         <div id="contact" className="contact">
-            <h1 className="contactTitle">Contact me!</h1>
+            <h1 className="contactTitle">Contact me</h1>
             <div className="contacts">
                 <h3>domino2k1@gmail.com</h3>
                 {/* <h3>phone</h3> */}
             </div>
             <div className="linkButtons">
                 <a href="https://github.com/ddmuzyk" className="linkButton">GITHUB</a>
-                <a href="https://pl-pl.facebook.com/" className="linkButton">FACEBOOK</a>
-                <a href="https://www.instagram.com/" className="linkButton">INSTAGRAM</a>
+                <a href="https://www.linkedin.com/in/dominik-muzyka/" className="linkButton">LINKEDIN</a>
             </div>
         </div>
     )
